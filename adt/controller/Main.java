fab037c6a9a563ee96103290f312825280ae8cf5
Biagio Rosario Greco <32863770+birg81@users.noreply.github.com>
2022-11-27 16:16:43 +0100

    Update Main.java

--- adt/controller/Main.java
+++ adt/controller/Main.java
@@ -4,17 +4,26 @@ import model.Stack;
 
 public class Main {
 	public static void main(String[] args) {
+		// Create a new Stack of String
 		Stack<String> stack = new Stack<>();
-		stack.push("lunedì");
-		stack.push("martedì");
-		stack.push("mercoledì");
-		stack.push("giovedì");
-		stack.push("venerdì");
-		stack.push("sabato");
-		stack.push("domenica");
+		// print a void stack
+		System.out.println(stack);
+		// popolate stack with many elements
+		stack.push("monday");
+		stack.push("tuesday");
+		stack.push("wednesday");
+		stack.push("thursday");
+		stack.push("friday");
+		// Remember: the buffer size have a limit of 5 elements!
+		stack.push("saturday");
+		stack.push("sunday");
+		// progressive removing elements while stack will be empty
 		while(!stack.isEmpty()) {
 			System.out.println(stack);
 			System.out.printf("%s (%d remains)\n", stack.pop(), stack.size());
 		}
+		// now stack is empty
+		System.out.println(stack);
+		System.out.printf("%s (%d remains)\n", stack.pop(), stack.size());
 	}
 }
